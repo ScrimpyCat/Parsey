31b5dda82c31ad3f3cc20d4c7d58a5ee3365008b
Stefan Johnson <sj@scrimpycat.io>
2016-03-30 06:24:41 +1100

    Initial commit

---
+++ lib/parsey.ex
@@ -0,0 +1,173 @@
+defmodule Parsey do
+    @moduledoc """
+      A library to setup basic parsing requirements for non-complex nested
+      inputs.
+
+      Parsing behaviours are defined using rulesets, these sets take the format
+      of <code class="inline">[<a href="#t:rule/0">rule</a>]</code>. Rulesets are
+      matched against in the order defined. The first rule in the set will have a
+      higher priority than the last rule in the set.
+
+      A <code class="inline"><a href="#t:rule/0">rule</a></code> is a matching
+      expression that is named. The name of a rule can be any atom, and multiple
+      rules can consist of the same name. While the matching expression can be
+      either a Regex expression or a function.
+
+      Rules may additionally be configured to specify the additional options that
+      will be returned in the <code class="inline"><a href="#t:ast/0">ast</a></code>,
+      or the ruleset modification behaviour (what rules to exclude, include or
+      re-define), and if the rule should be ignored (not added to the
+      <code class="inline"><a href="#t:ast/0">ast</a></code>).
+
+      The default behaviour of a matched rule is to remove all rules with the same
+      name from the ruleset, and then try further match the matched input with the
+      new ruleset. Returning the <code class="inline"><a href="#t:ast/0">ast</a></code>
+      one completion.
+
+      The behaviour of matchers (applies to both regex and functions) is return a
+      list of indices `[{ index, length }]` where the first `List.first` tuple in
+      the list is used to indicate the portion of the input to be removed, while
+      the last `List.last` is used to indicate the portion of the input to be
+      focused on (parsed further).
+    """
+
+    @type name :: atom
+    @type matcher :: Regex.t | (String.t -> (nil | [{ integer, integer }]))
+    @type option :: any
+    @type excluder :: name | { name, option }
+    @type rule :: { name, matcher } | { name, %{ :match => matcher, :option => option, :ignore => boolean, :exclude => excluder | [excluder], :include => rule | [rule], :rules => rule | [rule] } }
+    @type ast :: String.t | { name, [ast] } | { name, [ast], option }
+
+    @doc """
+      Parse the given input using the specified ruleset.
+
+      Example
+      -------
+        iex> rules = [
+        ...>     whitespace: %{ match: ~r/\\A\\s/, ignore: true },
+        ...>     element_end: %{ match: ~r/\\A<\\/.*?>/, ignore: true },
+        ...>     element: %{ match: fn
+        ...>         input = <<"<", _ :: binary>> ->
+        ...>             elements = String.splitter(input, "<", trim: true)
+        ...> 
+        ...>             [first] = Enum.take(elements, 1)
+        ...>             [{ 0, tag_length }] = Regex.run(~r/\\A.*?>/, first, return: :index)
+        ...>             tag_length = tag_length + 1
+        ...> 
+        ...>             { 0, length } = Stream.drop(elements, 1) |> Enum.reduce_while({ 1, 0 }, fn
+        ...>                 element = <<"/", _ :: binary>>, { 1, length } ->
+        ...>                     [{ 0, tag_length }] = Regex.run(~r/\\A.*?>/, element, return: :index)
+        ...>                     { :halt, { 0, length + tag_length + 1 } }
+        ...>                 element = <<"/", _ :: binary>>, { count, length } -> { :cont, { count - 1, length + String.length(element) + 1 } }
+        ...>                 element, { count, length } -> { :cont, { count + 1, length + String.length(element) + 1 } }
+        ...>             end)
+        ...> 
+        ...>             length = length + String.length(first) + 1
+        ...>             [{ 0, length }, {1, tag_length - 2}, { tag_length, length - tag_length }]
+        ...>         _ -> nil
+        ...>     end, exclude: nil, option: fn input, [_, { index, length }, _] -> String.slice(input, index, length) end },
+        ...>     value: %{ match: ~r/\\A\\d+/, rules: [] }
+        ...> ]
+        iex> input = \"\"\"
+        ...> <array>
+        ...>     <integer>1</integer>
+        ...>     <integer>2</integer>
+        ...> </array>
+        ...> <array>
+        ...>     <integer>3</integer>
+        ...>     <integer>4</integer>
+        ...> </array>
+        ...> \"\"\"
+        iex> Parsey.parse(input, rules)
+        [
+            { :element, [
+                { :element, [value: ["1"]], "integer" },
+                { :element, [value: ["2"]], "integer" }
+            ], "array" },
+            { :element, [
+                { :element, [value: ["3"]], "integer" },
+                { :element, [value: ["4"]], "integer" }
+            ], "array" },
+        ]
+    """
+    @spec parse(String.t, [rule]) :: [ast]
+    def parse(input, rules), do: parse(input, rules, [])
+
+    @doc false
+    @spec parse(String.t, [rule], [ast]) :: [ast]
+    defp parse("", _, nodes), do: Enum.reverse(nodes)
+    defp parse(input, rules, [string|nodes]) when is_binary(string) do
+        case get_node(input, rules) do
+            { next, node } -> parse(next, rules, [node, string|nodes])
+            nil -> parse(String.slice(input, 1..-1), rules, [string <> String.first(input)|nodes])
+        end
+    end
+    defp parse(input, rules, nodes) do
+        case get_node(input, rules) do
+            { next, nil } -> parse(next, rules, nodes)
+            { next, node } -> parse(next, rules, [node|nodes])
+            nil -> parse(String.slice(input, 1..-1), rules, [String.first(input)|nodes])
+        end
+    end
+
+    @doc false
+    @spec get_node(String.t, [rule]) :: { String.t, ast | nil } | nil
+    defp get_node(input, rules) do
+        Enum.find_value(rules, fn
+            rule = { _, regex = %Regex{} } -> make_node(input, rule, Regex.run(regex, input, return: :index), rules)
+            rule = { _, %{ match: regex = %Regex{} } } -> make_node(input, rule, Regex.run(regex, input, return: :index), rules)
+            rule = { _, %{ match: func } } -> make_node(input, rule, func.(input), rules)
+            rule = { _, func } -> make_node(input, rule, func.(input), rules)
+        end)
+    end
+
+    @doc false
+    @spec make_node(String.t, rule, nil | [{ integer, integer }], [rule]) :: { String.t, ast } | nil
+    defp make_node(_, _, nil, _), do: nil
+    defp make_node(input, rule, indexes, rules) do
+        { index, length } = List.last(indexes)
+        { entire_index, entire_length } = List.first(indexes)
+        { String.slice(input, (entire_index + entire_length)..-1), node(String.slice(input, index, length), rule, remove_rules(rules, rule) |> include_rules(rule) |> replace_rules(rule), input, indexes) }
+    end
+
+    @doc false
+    @spec node(String.t, rule, [rule], String.t, [{ integer, integer }]) :: ast | nil
+    defp node(_, { _, %{ ignore: true } }, _, _, _), do: nil
+    defp node(input, { name, %{ option: option } }, rules, original, indexes) when is_function(option), do: { name, parse(input, rules), option.(original, indexes) }
+    defp node(input, { name, %{ option: option } }, rules, _, _), do: { name, parse(input, rules), option }
+    defp node(input, { name, _ }, rules, _, _), do: { name, parse(input, rules) }
+
+    @doc false
+    @spec remove_rules([rule], rule) :: [rule]
+    defp remove_rules(rules, { _, %{ exclude: { name, option } } }) do
+        Enum.filter(rules, fn
+            { rule, %{ option: rule_option } } ->
+                rule != name or rule_option != option
+            _ -> true
+        end)
+    end
+    defp remove_rules(rules, { _, %{ exclude: name } }) when is_atom(name), do: Enum.filter(rules, fn { rule, _ } -> rule != name end)
+    defp remove_rules(rules, { _, %{ exclude: names } }) do
+        Enum.filter(rules, fn
+            { rule, %{ option: rule_option } } -> !Enum.any?(names, fn
+                { name, option } -> rule == name and rule_option == option
+                _ -> false
+            end)
+            { rule, _ } -> !Enum.any?(names, fn
+                { _name, _option } -> false
+                name -> rule == name
+            end)
+        end)
+    end
+    defp remove_rules(rules, { name, _ }), do: Enum.filter(rules, fn { rule, _ } -> rule != name end)
+
+    @doc false
+    @spec include_rules([rule], rule) :: [rule]
+    defp include_rules(rules, { _, %{ include: new_rules } }), do: new_rules ++ rules
+    defp include_rules(rules, _), do: rules
+
+    @doc false
+    @spec replace_rules([rule], rule) :: [rule]
+    defp replace_rules(_, { _, %{ rules: new_rules } }), do: new_rules
+    defp replace_rules(rules, _), do: rules
+end
---
+++ test/parsey_test.exs
@@ -0,0 +1,112 @@
+defmodule ParseyTest do
+    use ExUnit.Case
+    doctest Parsey
+
+    test "parsing with no rules" do
+        assert ["test"] == Parsey.parse("test", [])
+    end
+
+    test "simple matching" do
+        assert [{ :consonant, ["t"] }, { :vowel, ["e"] }, { :consonant, ["st"] }] == Parsey.parse("test", [vowel: ~r/\A[aeiou]+/, consonant: ~r/\A[^aeiou]+/])
+        assert [{ :consonant, ["t"] }, { :vowel, ["e"] }, { :consonant, ["st"] }] == Parsey.parse("test", [vowel: %{ match: ~r/\A[aeiou]+/ }, consonant: %{ match: ~r/\A[^aeiou]+/ }])
+        assert [{ :bracket, [{ :consonant, ["t"] }, { :vowel, ["e"] }, { :consonant, ["st"] }] }] == Parsey.parse("(test)", [bracket: ~r/\A\((.*?)\)/, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A[^aeiou]+/])
+        assert ["t", { :vowel, ["e"] }, "st"] == Parsey.parse("test", [vowel: fn
+            <<"e", _ :: binary>> -> [{ 0, 1 }]
+            _ -> nil
+        end])
+    end
+
+    test "simple options" do
+        assert ["t", { :vowel, ["e"], :test }, "st"] == Parsey.parse("test", [vowel: %{ match: ~r/\A[aeiou]+/, option: :test }])
+        assert [{ :bracket, [{ :consonant, ["t"], :t }, { :vowel, ["e"] }, { :consonant, ["s"] }, { :consonant, ["t"], :t }] }] == Parsey.parse("(test)", [bracket: ~r/\A\((.*?)\)/, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A([^aeiout]+)/, consonant: %{ match: ~r/\A(t)/, option: :t }])
+        assert ["t", { :vowel, ["e"], { "est", [{ 0, 1 }] } }, "st"] == Parsey.parse("test", [vowel: %{ match: ~r/\A[aeiou]+/, option: &({ &1, &2 })}])
+    end
+
+    test "simple exclusion" do
+        assert [{ :bracket, ["t", { :vowel, ["e"] }] }, { :consonant, ["st"] }] == Parsey.parse("(te)st", [bracket: %{ match: ~r/\A\((.*?)\)/, exclude: :consonant }, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A[^aeiou]+/])
+        assert [{ :bracket, ["te", { :consonant, ["s"] }, "t"] }] == Parsey.parse("(test)", [bracket: %{ match: ~r/\A\((.*?)\)/, exclude: [{ :consonant, :t }, :vowel] }, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A([^aeiout]+)/, consonant: %{ match: ~r/\A(t)/, option: :t }])
+    end
+
+    test "simple inclusion" do
+        assert [{ :bracket, ["t", { :inner_vowel, ["e"] }, { :consonant, ["s"] }, "t"] }] == Parsey.parse("(test)", [bracket: %{ match: ~r/\A\((.*?)\)/, exclude: [{ :consonant, :t }, :vowel], include: [inner_vowel: ~r/\A[aeiou]+/] }, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A([^aeiout]+)/, consonant: %{ match: ~r/\A(t)/, option: :t }])
+    end
+
+    test "simple replace" do
+        assert [{ :bracket, ["test"] }] == Parsey.parse("(test)", [bracket: %{ match: ~r/\A\((.*?)\)/, rules: [] }, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A([^aeiout]+)/, consonant: %{ match: ~r/\A(t)/, option: :t }])
+        assert [{ :bracket, ["t", { :inner_vowel, ["e"] }, "st"] }] == Parsey.parse("(test)", [bracket: %{ match: ~r/\A\((.*?)\)/, rules: [inner_vowel: ~r/\A[aeiou]+/] }, vowel: ~r/\A[aeiou]+/, consonant: ~r/\A([^aeiout]+)/, consonant: %{ match: ~r/\A(t)/, option: :t }])
+    end
+
+    test "simple ignore" do
+        assert [{ :consonant, ["t"] }, { :consonant, ["st"] }] == Parsey.parse("test", [vowel: %{ match: ~r/\A[aeiou]+/, ignore: true }, consonant: ~r/\A[^aeiou]+/])
+    end
+
+    test "complex parsing Lisp-like" do
+        rules = [
+            whitespace: %{ match: ~r/\A\s/, ignore: true },
+            expression: %{ match: ~r/\A\((.*)\)/, exclude: nil },
+            integer: %{ match: ~r/\A\d+/, rules: [] },
+            atom: %{ match: ~r/\A\S+/, rules: [] }
+        ]
+
+        assert [
+            expression: [
+                atom: ["+"],
+                expression: [
+                    atom: ["+"],
+                    integer: ["1"],
+                    integer: ["2"]
+                ],
+                integer: ["15"]
+            ]
+        ] == Parsey.parse("(+ (+ 1 2) 15)", rules)
+    end
+
+    test "complex parsing XML-like" do
+        rules = [
+            whitespace: %{ match: ~r/\A\s/, ignore: true },
+            element_end: %{ match: ~r/\A<\/.*?>/, ignore: true },
+            element: %{ match: fn
+                input = <<"<", _ :: binary>> ->
+                    elements = String.splitter(input, "<", trim: true)
+
+                    [first] = Enum.take(elements, 1)
+                    [{ 0, tag_length }] = Regex.run(~r/\A.*?>/, first, return: :index)
+                    tag_length = tag_length + 1
+
+                    { 0, length } = Stream.drop(elements, 1) |> Enum.reduce_while({ 1, 0 }, fn
+                        element = <<"/", _ :: binary>>, { 1, length } ->
+                            [{ 0, tag_length }] = Regex.run(~r/\A.*?>/, element, return: :index)
+                            { :halt, { 0, length + tag_length + 1 } }
+                        element = <<"/", _ :: binary>>, { count, length } -> { :cont, { count - 1, length + String.length(element) + 1 } }
+                        element, { count, length } -> { :cont, { count + 1, length + String.length(element) + 1 } }
+                    end)
+
+                    length = length + String.length(first) + 1
+                    [{ 0, length }, {1, tag_length - 2}, { tag_length, length - tag_length }]
+                _ -> nil
+            end, exclude: nil, option: fn input, [_, { index, length }, _] -> String.slice(input, index, length) end },
+            value: %{ match: ~r/\A\d+/, rules: [] }
+        ]
+        source = """
+        <array>
+            <integer>1</integer>
+            <integer>2</integer>
+        </array>
+        <array>
+            <integer>3</integer>
+            <integer>4</integer>
+        </array>
+        """
+
+        assert [
+            { :element, [
+                { :element, [value: ["1"]], "integer" },
+                { :element, [value: ["2"]], "integer" }
+            ], "array" },
+            { :element, [
+                { :element, [value: ["3"]], "integer" },
+                { :element, [value: ["4"]], "integer" }
+            ], "array" },
+        ] == Parsey.parse(source, rules)
+    end
+end
